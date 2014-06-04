3acad395bab5b1e0c8519ef89c86992e174d0f42
Zoe Rooney <hello@zoerooney.com>
2014-06-03 23:27:15 -0400

    add checkout files to sass

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,25 @@ var gulp = require('gulp'),
     server = lr();
     
 gulp.task('default', function(){
-	return gulp.src('assets/_scss/style.scss', {base: 'assets/_scss'})
-	    .pipe(plumber())
-	    .pipe(sass({ style: 'expanded' }))
-	    .pipe(gulp.dest('assets/'))
-	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	    .pipe(minifycss())
-	    .pipe(gulp.dest('assets/'))
-	    .pipe(livereload(server));
+  return gulp.src('assets/_scss/style.scss', {base: 'assets/_scss'})
+      .pipe(plumber())
+      .pipe(sass({ style: 'expanded' }))
+      .pipe(gulp.dest('assets/'))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+      .pipe(minifycss())
+      .pipe(gulp.dest('assets/'))
+      .pipe(livereload(server));
 });
-
+gulp.task('checkout', function(){
+  return gulp.src('assets/_scss/checkout.scss')
+      .pipe(plumber())
+      .pipe(sass({ style: 'expanded' }))
+      .pipe(gulp.dest('assets/'))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+      .pipe(minifycss())
+      .pipe(gulp.dest('assets/'))
+      .pipe(livereload(server));
+});
 gulp.task('watch', function() {
   // Listen on port 35729
   server.listen(35729, function (err) {
@@ -29,7 +38,7 @@ gulp.task('watch', function() {
   
       // Watch .scss files
       gulp.watch('assets/**/*.scss', ['default']);
-  
+      gulp.watch('assets/**/_checkout.scss', ['checkout']);
     });
 
 });
