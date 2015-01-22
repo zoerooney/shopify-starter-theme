c57f1c21e77c1f0df1a0c7a03a54936895dc9199
Zoe Rooney <hello@zoerooney.com>
2015-01-22 15:45:39 -0500

    updated for new version of gulp-ruby-sass; remove checkout sass files due to new checkout

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,23 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
+
 function replaceWithAssetURL( match, imgurl){
   return 'background:url({{ "'+ imgurl + '" | asset_url }})';
-}    
+}   
+
 gulp.task('styles', function() {
-  return gulp.src(['assets/_scss/*.scss',
-                   'assets/_scss/**/*.scss',
-                   '!assets/_scss/checkout.scss',
-                   '!assets/_scss/pages/_checkout.scss'], 
-                   {base:  'assets/_scss'})
-      .pipe(plumber())
-      .pipe(sass())
+  return sass('assets/_scss/') 
+      .on('error', function (err) {
+          console.error('Error!', err.message);
+      })
       .pipe(autoprefixer())
       .pipe(minifycss())
       .pipe(rename({extname: '.css.liquid'}))
       .pipe(replace(/background:url\((.*?)\)/g, replaceWithAssetURL))
       .pipe(gulp.dest('assets/'));
 });
-gulp.task('checkout', function() {
-  return gulp.src(['assets/_scss/*.scss',
-                   'assets/_scss/**/*.scss',
-                   '!assets/_scss/style.scss'], 
-                   {base:  'assets/_scss'})
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(minifycss())
-      .pipe(rename({extname: '.scss.liquid'}))
-      .pipe(gulp.dest('assets/'));
-});
+
 gulp.task('watch', function() {
   // Listen on port 35729
   server.listen(35729, function (err) {
@@ -47,8 +35,7 @@ gulp.task('watch', function() {
       };
   
       // Watch .scss files
-      gulp.watch(['assets/_scss/*.scss','assets/_scss/**/*.scss','!assets/_scss/checkout.scss','!assets/_scss/pages/_checkout.scss'], ['styles']);
-      gulp.watch('assets/_scss/pages/_checkout.scss', ['checkout']);
+      gulp.watch(['assets/_scss/*.scss','assets/_scss/**/*.scss'], ['styles']);
     });
 
 });
